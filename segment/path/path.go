8616c6b08ab2e00a162edd9bc5088e1bfb673aa6
Tobias Bales <tobias.bales@gmail.com>
2017-09-18 06:52:50 +0200

    feat: highlight github user and branch differently

--- segment/path/path.go
+++ segment/path/path.go
@@ -13,7 +13,7 @@ type path struct {
 }
 
 func (p *path) Value() chan string {
-	gh := regexp.MustCompile("(.*github.com/)([a-zA-Z0-9-_]*)/([a-zA-Z0-9-_]*)(/?.*)")
+	gh := regexp.MustCompile("(.*github.com/)([a-zA-Z0-9-_]*)(/[a-zA-Z0-9-_]*)?(/?.*)?")
 	c := make(chan string)
 
 	go func() {
@@ -24,7 +24,16 @@ func (p *path) Value() chan string {
 		}
 
 		m := gh.FindStringSubmatch(p)
-		c <- m[1] + aurora.Cyan(m[2]).String() + "/" + aurora.Red(m[3]).String() + m[4] + " "
+		p = m[1] + aurora.Cyan(m[2]).String()
+		if len(m) > 4 && m[3] != "" {
+			p = p + "/" + aurora.Red(m[3][1:]).String()
+		}
+		if len(m) > 5 && m[4] != "" {
+			p = p + m[4]
+		}
+		p = p + " "
+
+		c <- p
 	}()
 
 	return c
